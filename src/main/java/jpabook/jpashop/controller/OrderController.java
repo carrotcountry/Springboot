130e10db69c66a3555edcc1ac461b920ab2228e5
carrotcountry <jaeyoon321@naver.com>
2023-05-07 01:17:12 +0900

    Show orderList

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -9,7 +9,9 @@ import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -41,18 +43,33 @@ public class OrderController {
 
     @ResponseBody
     @RequestMapping (value = "/order", method = RequestMethod.POST)
-    public String order(@RequestBody String json) {
+    public String order(@RequestBody String json) { //String json
         try {
-            System.out.println("json : " + json);
             JSONArray array = new JSONArray(json);
+            System.out.println(json);
 
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject jsonObject = (JSONObject)array.get(i);
 
+                //orderService에 넘길 멤버 Id
+                String MemberId = (String) jsonObject.get("memberId");
+                Long MemberIdToInt = Long.parseLong(MemberId);
+
+                //orderService에 넘길 아이템 Id
+                String itemId = (String) jsonObject.get("itemId");
+                Long itemIdToInt = Long.parseLong(itemId);
+
+                //orderService에 넘길 주문 수량
+                String count = (String) jsonObject.get("count");
+                int OrderCount = Integer.parseInt(count);
+
+                orderService.order(MemberIdToInt, itemIdToInt, OrderCount);
+            }
             return "redirect:/orders";
         } catch (Exception e) {
             e.printStackTrace();
             return "FAIL";
         }
-
     }
 
     @GetMapping("/orders")
@@ -62,12 +79,4 @@ public class OrderController {
 
         return "order/orderList";
     }
-//    @PutMapping("/orders/{name}")
-//    public String putOrder()
-
-
-
-
 }
-
-
